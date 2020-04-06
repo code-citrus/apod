40f28760b5ac8dc3f759b0a9ccf8eca889cc89bb
gautham <gautham.g@live.com>
2020-04-06 10:18:51 +0200

    Basic POC

---
+++ src/index.js
@@ -0,0 +1,36 @@
+(function() {
+  var APOD_URL = 'https://api.nasa.gov/planetary/apod';
+  var API_KEY = 'H2M20c7MB1DTGzdREfIrCGBQY2hH6NaLjZ2uKatj';
+
+  var callAPOD = new Promise((resolve, reject) => {
+    var url = APOD_URL + '?api_key=' + API_KEY;
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('GET', url);
+    xhr.onload = function() {
+      if(xhr.status != 200) {
+        throw new Error(`${xhr.status}: ${xhr.statusText}`)
+      }
+      if(!xhr.response) {
+        throw new Error('Empty xhr response');
+      }
+      resolve(xhr.response);
+    }
+    xhr.send();
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    callAPOD.then((json) => {
+      var div = document.createElement('div');
+      var image = document.createElement('img');
+      image.src = json.url;
+      var paragraph = document.createElement('p');
+      paragraph.textContent = json.explanation;
+      var body = document.getElementsByTagName('body')[0];
+      div.appendChild(image);
+      div.appendChild(paragraph);
+      body.appendChild(div);
+    });
+  });
+})();
