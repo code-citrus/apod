85bd4fd9c98afcd134ac436afe9d09cd0c99e554
gautham <gautham.g@live.com>
2020-04-07 12:03:51 +0200

    scss support

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  mode: 'development',
+
+  devServer: {
+
+    // Tell webpack-dev-server to trigger full reload
+    // when files in contentBase changes (as opposed
+    // to only when JS dependency are modified).
+    watchContentBase: true,
+    
+    // Serve static files from here. Note that
+    // the normal webpack output will *not* show
+    // up in dist as they are held in a virtual dir
+    // during this time.
+    contentBase: './dist',
+    
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'index.[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+
+}
